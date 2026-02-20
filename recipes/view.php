2fbf0e898c6461f070b107b921e992b60a54d932
Nikolay <niki.radni@gmail.com>
2026-02-20 12:14:13 +0200

    Added first css steps

--- recipes/view.php
+++ recipes/view.php
@@ -4,22 +4,47 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>recipes</title>
+        <link rel="stylesheet" href="/sources/style.css">
     </head>
     <body>
-        <h1>Recipes</h1>
-        <ul>
-            <?php foreach ($recipe_titles as $index => $recipe) { ?>
-            <li><a href = "/recipe/?id=<?php echo $index; ?>">
-            	<?php echo $recipe; ?>
-                </a></li>
-            <?php } ?>
-        </ul>
-		<a href = "/add_recipe/"><h2>add</h2></a>
-        <?php 
-            session_start();
-            if(array_key_exists('status', $_SESSION) && $_SESSION['status'] == "loged in"){
-                echo "<a href = '/login/'>Log out</a>";
-            }
-        ?>
+        <div class="container1">
+            <header>
+            <h1>Recipes</h1>
+                                <nav>
+                    <a href="*">COMPANY</a>
+                    <a href="*">SERVICES</a>
+                    <a href="*">TEAM</a>
+                    <a href="*">CONTACTS</a>
+                </nav>
+                    <button class="btn-signin1">Sign In</button>
+            </header>   
+            <main>
+                <div class="content">
+                <?php foreach ($recipe_titles as $index => $recipe) { ?>
+                <div class="recipe-card">
+                    <div class="horizontal">
+                        <div class="text-side">
+                            <div class="tag-box">
+                                <a href="/recipe/?id=<?php echo $index; ?>" class="recipe-link">
+                                    <div class="tag"><?php echo $recipe; ?></div>
+                                </a>
+                            </div>
+                                <p class="description">
+                                    This is a short description of the recipe.
+                                    You can later replace this with real data from your database.
+                                </p>
+                            </div>
+                        <spline-viewer class="robot-3d"
+                            url="https://prod.spline.design/GMeXEYiP5RaVcQ-Q/scene.splinecode">
+                        </spline-viewer>
+                        
+                    </div>
+                </div>
+                <?php } ?>
+                <a href = "/add_recipe/"><h2>add</h2></a>
+                </div>
+            </main>
+        </div>
+        <script type="module" src="https://unpkg.com/@splinetool/viewer@1.12.58/build/spline-viewer.js"></script>
 	</body>
 </html>
